b3e7453564815c3d99c48301682749de2afab698
月夜 <18012011131@163.com>
2019-04-29 11:45:17 +0800

    '优化'

--- src/view/business/list/xinhongqiao.vue
+++ src/view/business/list/xinhongqiao.vue
@@ -31,13 +31,13 @@
   </div>
 </template>
 <script>
-import Tables from "_c/tables";
-import { getTradingAreaList } from "@/api/data";
-import { postdelTradingArea } from "@/api/data";
-import { formatDate } from "@/libs/formatdate";
+import Tables from '_c/tables'
+import { getTradingAreaList } from '@/api/data'
+import { postdelTradingArea } from '@/api/data'
+import { formatDate } from '@/libs/formatdate'
 
 export default {
-  name: "wechatlist",
+  name: 'wechatlist',
   components: {
     Tables
   },
@@ -45,10 +45,10 @@ export default {
   props: {
     busnissId: {
       type: String,
-      default: "0"
+      default: '0'
     }
   },
-  data() {
+  data () {
     return {
       // 分页
       current: 1,
@@ -56,189 +56,189 @@ export default {
       // 表格
       columns: [
         {
-          title: "商场名称",
-          key: "name",
-          className: "tabletitle",
+          title: '商场名称',
+          key: 'name',
+          className: 'tabletitle',
           render: (h, params) => {
             // console.log(params.row.title);
             return h(
-              "a",
+              'a',
               {
                 on: {
                   click: () => {
-                    this.goMarket(params.row.id);
+                    this.goMarket(params.row.id)
                   }
                 }
               },
               params.row.name
-            );
+            )
           }
         },
         {
-          title: "商场封面",
-          key: "url",
+          title: '商场封面',
+          key: 'url',
           render: (h, params) => {
             // console.log(params.row.title);
-            return h("img", {
+            return h('img', {
               attrs: {
                 src: params.row.url
               },
               style: {
-                height: "80px",
-                "margin-top": "5px"
+                height: '80px',
+                'margin-top': '5px'
               }
-            });
+            })
           }
         },
-        { title: "地图经纬度", key: "longitude" },
-        { title: "描述", key: "description" },
+        { title: '地图经纬度', key: 'longitude' },
+        { title: '描述', key: 'description', ellipsis: true },
         {
-          title: "创建时间",
-          key: "createDate",
+          title: '创建时间',
+          key: 'createDate',
           render: (h, params) => {
             return h(
-              "div",
-              formatDate(new Date(params.row.createDate), "yyyy-MM-dd hh:mm")
-            );
+              'div',
+              formatDate(new Date(params.row.createDate), 'yyyy-MM-dd hh:mm')
+            )
           }
         },
         {
-          title: "操作",
-          key: "action",
+          title: '操作',
+          key: 'action',
           width: 250,
-          align: "center",
-          searchable: "false",
+          align: 'center',
+          searchable: 'false',
           render: (h, params) => {
-            return h("div", [
+            return h('div', [
               h(
-                "Button",
+                'Button',
                 {
                   props: {
-                    type: "primary",
-                    size: "small"
+                    type: 'primary',
+                    size: 'small'
                   },
                   style: {
-                    marginRight: "5px"
+                    marginRight: '5px'
                   },
                   on: {
                     click: () => {
-                      this.show(params);
+                      this.show(params)
                     }
                   }
                 },
-                "查看详情"
+                '查看详情'
               ),
               h(
-                "Button",
+                'Button',
                 {
                   props: {
-                    type: "warning",
-                    size: "small"
+                    type: 'warning',
+                    size: 'small'
                   },
                   style: {
-                    marginRight: "5px"
+                    marginRight: '5px'
                   },
                   on: {
                     click: () => {
-                      this.updateInfo(params);
+                      this.updateInfo(params)
                     }
                   }
                 },
-                "修改"
+                '修改'
               ),
               h(
-                "Button",
+                'Button',
                 {
                   props: {
-                    type: "error",
-                    size: "small"
+                    type: 'error',
+                    size: 'small'
                   },
                   style: {
-                    marginRight: "5px"
+                    marginRight: '5px'
                   },
                   on: {
                     click: () => {
-                      this.remove(params);
+                      this.remove(params)
                     }
                   }
                 },
-                "删除"
+                '删除'
               )
-            ]);
+            ])
           }
         }
       ],
       tableData: []
-    };
+    }
   },
   methods: {
-    exportExcel() {
+    exportExcel () {
       // 导出csv
       this.$refs.tables.exportCsv({
         filename: `table-${new Date().valueOf()}.csv`
-      });
+      })
     },
-    changePage(event) {
+    changePage (event) {
       // 分页
-      console.log(event);
-      this.getTradingAreaListFun(event);
+      console.log(event)
+      this.getTradingAreaListFun(event)
     },
-    show(params) {
-      console.log(params.row.content);
+    show (params) {
+      console.log(params.row.content)
       // 查看
       this.$Modal.info({
         title: params.row.name,
         scrollable: true,
         closable: true,
         content: params.row.content
-      });
+      })
     },
-    remove(params) {
+    remove (params) {
       // 删除
-      console.log(params.row.id, params);
+      console.log(params.row.id, params)
       // return;
       postdelTradingArea({
         id: params.row.id
       }).then(res => {
-        this.tableData.splice(params.index, 1);
-      });
+        this.tableData.splice(params.index, 1)
+      })
     },
-    updateInfo(query) {
+    updateInfo (query) {
       // 修改页面
-      console.log(query);
+      console.log(query)
       this.$router.push({
-        path: "/busnissUpdate",
+        path: '/busnissUpdate',
         query: { shopData: query.row }
-      });
+      })
     },
-    goMarket(index) {
-      console.log(index);
-      this.$router.push({ path: "/active", query: { tradingAreaId: index } });
+    goMarket (index) {
+      console.log(index)
+      this.$router.push({ path: '/active', query: { tradingAreaId: index } })
     },
     // 去添加
-    routerPushAddActiveInfo() {
+    routerPushAddActiveInfo () {
       this.$router.push({
-        path: "/busnissAdd",
+        path: '/busnissAdd',
         query: { busnissId: 1 }
-      });
+      })
     },
-    getTradingAreaListFun(pageNo) {
-      var that = this;
+    getTradingAreaListFun (pageNo) {
+      var that = this
       getTradingAreaList({
-        type:1,
+        type: 1,
         isShoppingMall: 0,
         pageSize: 15,
         pageNo
       }).then(res => {
-        that.tableData = res.data.data.parameterType;
+        that.tableData = res.data.data.parameterType
         // console.log(res);
-      });
+      })
     }
   },
-  mounted() {
-    this.getTradingAreaListFun(1);
+  mounted () {
+    this.getTradingAreaListFun(1)
   }
-};
+}
 </script>
 <style lang="less">
 tbody {
